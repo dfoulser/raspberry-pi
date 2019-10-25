20fc6781c09694961e9b3161225a54a0de75760f
Dave Foulser <dfoulser@gmail.com>
2019-10-25 15:48:08 -0400

    Read samples without sleep() at >3300Hz
    The Python code uses get_last_result() to read from ADS1015 without
    calling sleep(). It just reads as fast as possible.
    
    This code prints out info on the timing of the read loop.
    
    The /boot/config.txt file includes the I2C config and baud rate.
    Default baud rate (100kbps) is set by raspi-config cmd line tool.
    It limits I2C reading from the ADS1015 to about 1600Hz.
    Increasing baud rate to 400kbps gives about 250usec read (down from 600+)
    and increasing to 1Mbps gives about 170usec read (~6000Hz).
    Increasing baud rate to 2Mbps or 3Mbps causes IO Error, so not used.
    
    At this point I can read from ADC at faster than 3300Hz, so now to
    stabilize that reading at 3300Hz with low jitter.
    
    I'm still wondering what will happen when i try to read 2 ADC's,
    will I be able to do this with threading or multi-processing, so that
    both read easily.

--- src/record_data.py
+++ src/record_data.py
@@ -6,12 +6,23 @@ import argparse
 import numpy as np
 # Import the sound library for read/write WAV files (also compressed MP3)
 import soundfile
+import time
 
-int SAMPLE_RATE = 3300  # 3300 Hz is fastest the ADS1015 can read
-int DURATION_SECONDS = 60  # one minutes
-int CHANNEL = 0  # wire all devices to A0 channel
-int NUM_DEVICES = 1  # working with a single ADC now, later read this from the hardware itself
-str SOUNDFILE_PATTERN = "%s_%4d%2d%2d_%02d%02d.wav"
+DATA_RATE = 3300  # 3300 Hz is fastest the ADS1015 can read
+DURATION_SECONDS = 60  # one minutes
+CHANNEL = 0  # wire all devices to A0 channel
+NUM_DEVICES = 1  # working with a single ADC now, later read this from the hardware itself
+SOUNDFILE_PATTERN = "%s_%4d%2d%2d_%02d%02d.wav"
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 2
 
 def handleArgs():
     parser = argparse.ArgumentParser(description='Record data from Mic+ADC.')
@@ -24,7 +35,7 @@ def handleArgs():
     return args
 
 def allocateSamplesArray():
-    array = np.zeros((NUM_DEVICES, SAMPLE_RATE * DURATION_SECONDS), dtype=int16 )
+    array = np.zeros((NUM_DEVICES, DATA_RATE * DURATION_SECONDS), dtype=np.int16 )
     # Channels of sound are stored as rows in 2D numpy array.  Each
     # channel comes from one of the ADC devices.
     return array
@@ -44,15 +55,18 @@ def setupAdc():
     #  -   8 = +/-0.512V
     #  -  16 = +/-0.256V
     # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
-    GAIN = 2
+    #GAIN = GAIN (2)
 
     # Data rate goes up to 3300 with ADS1015. Default is 1600.
-    DATA_RATE = SAMPLE_RATE
+    # DATA_RATE = DATA_RATE
 
     # Start continuous ADC conversions on selected channel using the previously set gain
     # value.  Note you can also pass an optional data_rate parameter, see the simpletest.py
     # example and read_adc function for more infromation.
+    print ('CHANNEL = %d, gain = %d, data_rate = %d' % (CHANNEL, GAIN, DATA_RATE))
     adc.start_adc(CHANNEL, gain=GAIN, data_rate=DATA_RATE)
+    #adc.start_adc(CHANNEL, gain=GAIN)  # Let data_rate default to 1600.
+
     # Once continuous ADC conversions are started you can call get_last_result() to
     # retrieve the latest result, or stop_adc() to stop conversions.
     return adc
@@ -62,24 +76,42 @@ def stopAdc(adc):
     adc.stop_adc()
 
 def readData(adcs, samplesArray, data_rate=DATA_RATE, duration=DURATION_SECONDS):
-    print('Reading %d ADS1015 channel 0 for %d seconds...', adcs.shape()[0], duration)
+    print('Reading %d ADS1015 channel 0 for %d seconds...', len(adcs), duration)
     start = time.time()
-    timeStamp = start
+    readTime = start
     sample = 0
     SAMPLE_COUNT = duration * data_rate
     interval = 1.0/DATA_RATE
-    while sample < SAMPLE_COUNT && timeStamp < (start + interval * SAMPLE_COUNT):
-        for i in range(adcs.shape()[0]):
+    print('Reading %d samples at %f interval starting at %f' % 
+        (SAMPLE_COUNT, interval, start))
+    numAdcs = len(adcs)
+    print('Number of ADCs = %d' % numAdcs)
+    while (sample < SAMPLE_COUNT) and (readTime < (start + interval * SAMPLE_COUNT)):
+        readStart = time.time()
+        for i in range(numAdcs):
             # Read the last ADC conversion value and save it.
             samplesArray[i, sample] = adcs[i].get_last_result()
+            # Don't use get_last_result() as it seems to default to 1600Hz.  Try passing
+            # the data rate explicitly.
+            #samplesArray[i, sample] = adcs[i].read_adc(CHANNEL, gain=GAIN, data_rate=DATA_RATE)
+        readEnd = time.time()
+        print('Read duration = %f = (%f - %f)' % (readEnd - readStart, readEnd, readStart))
+
         # WARNING! If you try to read any other channel of this ADC during this continuous
         # conversion (like by calling read_adc again) it will disable the
         # continuous conversion!
         # print('Channel 0: {0}'.format(value))
-        # Sleep for rest of the interval (until next timeStamp)
+        # Sleep for rest of the interval (until next read time)
         sample = sample + 1
-        timeStamp = timeStamp + interval
-        time.sleep(timeStamp - time.time())
+        readTime = readTime + interval
+        #now = time.time()
+        #sleepInterval = readTime - now
+        #if sleepInterval > 0: 
+        #    print('Sleeping for %f seconds to time %f from now=%f' % (sleepInterval, readTime, now))
+        #    time.sleep(sleepInterval)
+        ##else:
+        ##    print('Not sleeping negative %f seconds to time %f from now=%f' % (sleepInterval, readTime, now))
+
 
 def getDatetimeFilename(prefix):
     timeStamp = time.time()
@@ -89,9 +121,11 @@ def getDatetimeFilename(prefix):
 
 def main():
     args = handleArgs()
+    # Create the list of ADC objects and allocate them to correspond
+    # to the ADC hardware we have.
     adcs = []
     for i in range(NUM_DEVICES):
-        adcs[i] = setupAdc()
+        adcs.append(setupAdc())
     samplesArray = allocateSamplesArray()
     # Read the data
     readData(adcs, samplesArray)
